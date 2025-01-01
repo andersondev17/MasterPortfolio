5f645eae766a9decdfd0b685f1ad83d9d04bbf1c
Anderson Lopez <52222621+TheGodFatherpte@users.noreply.github.com>
2024-12-31 21:57:29 -0500

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,160 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useEffect, useState } from 'react';
+import { TiLocationArrow } from 'react-icons/ti';
+import Button from './ui/Button';
+
+gsap.registerPlugin(ScrollTrigger)
+const Hero = () => {
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [hasClicked, setHasClicked] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedVideos, setLoadedVideos] = useState(0);
+
+    const totalVideos = 3;
+    const nexVideoRef = React.createRef(null);
+    // 0 % 4 = 0 + 1 => 1
+    // 1 % 4 = 1 + 1 => 2
+    // 2 % 4 = 2 + 1 => 3
+    // 3 % 4 = 3 + 1 => 4
+    // 4 % 4 = 0 + 1 => 1
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+    const handleMiniVdClick = () => {
+        setHasClicked(true);
+
+        setCurrentIndex(upcomingVideoIndex);
+    }
+    useEffect(() => {
+        if (loadedVideos === totalVideos - 1) {
+            setIsLoading(false)
+        }
+    }, [loadedVideos])
+
+    useGSAP(() => {
+        if (hasClicked) {
+            gsap.set('#next-video', { visibility: 'visible' });
+
+            gsap.to('#next-video', {
+                transformOrigin: 'center center',
+                scale: 1,
+                width: '100%',
+                height: '100%',
+                duration: 0.1,
+                ease: 'power1.inOut',
+                onStart: () => { nexVideoRef.current.play() },
+            })
+            gsap.to('#current-video', {
+                transformOrigin: 'center center',
+                scale: 0,
+                duration: 1.5,
+                ease: 'power1.inOut',
+                onStart: () => { nexVideoRef.current.play() },
+            })
+        }
+
+    }, { dependencies: [currentIndex], revertOnUpdate: true })
+
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            borderRadius: '0 0 40% 10%',
+        })
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            borderRadius: '0 0 0 0',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'top top',
+                end: 'bottom center',
+                scrub: true,
+            }
+        })
+    })
+    const getVideoSrc = (index) => {
+        return `videos/hero-${index}.mp4`;
+    };
+
+    const handleVideoLoaded = () => {
+        setLoadedVideos((prev) => prev + 1);
+    };
+
+    return (
+        <div className='relative h-dvh w-screen overflow-x-hidden'>
+            {isLoading && (
+                <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50 '>{/* wait unitl videos load */}
+                    <div className='three-body'>
+                        <div className='three-body__dot' />
+                        <div className='three-body__dot' />
+                        <div className='three-body__dot' />
+
+                    </div>
+                </div>
+
+            )}
+
+            <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-x-hidden rounded-lg bg-blue-75'>
+                <div>
+                    <div className='mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+                        <div onClick={handleMiniVdClick}
+                            className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
+
+                            <video
+                                ref={nexVideoRef}
+                                src={getVideoSrc(upcomingVideoIndex)}
+                                loop
+                                muted
+                                id='current-video'
+                                className='size-64 origin-center scale-150 object-cover object-center'
+                                onLoadedData={handleVideoLoaded}
+                            />
+                        </div>
+                    </div>
+
+                    <video
+                        ref={nexVideoRef}
+                        src={getVideoSrc(currentIndex)}
+                        loop
+                        muted
+                        id='next-video'
+                        className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+                        onLoadedData={handleVideoLoaded}
+                    />
+                    <video
+                        src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                        autoPlay
+                        loop
+                        muted
+                        id='current-video'
+                        className='absolute left-0 top-0 size-full object-cover object-center'
+                        onLoadedData={handleVideoLoaded}
+                    />
+                </div>
+                <h1 className=' special-font hero-heading absolute bottom-5 right-5  text-black'>
+                    G<b>a</b>ming
+                </h1>
+                <div className='absolute left-0 top-0 z-40 size-full'>
+                    <div className='mt-24 px-5 sm:px-10'>
+                        <h1 className='special-font hero-heading text-blue-100'>
+                            redifi<b>n</b>e
+                        </h1>
+                        <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
+                            Enter de Metagame layer <br />
+                            Unleash the Play Econommy
+                        </p>
+                        <Button id="watch-trailer"
+                            title="Watch Trailer"
+                            leftIcon={<TiLocationArrow />}
+                            containerClass="bg-yellow-300 flex-center gap-1"
+                        />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
